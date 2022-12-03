9f25b476714e9e93234150d830af5f62bcc52261
Oxel40 <Oxel40@users.noreply.github.com>
2022-12-03 12:24:09 +0100

    Create iconic-export-dependencies.bat

---
+++ iconic-export-dependencies.bat
@@ -0,0 +1,2 @@
+vcpkg export <pkg1> <pkg2> ... --[options]
+.\vcpkg.exe export --triplet x64-windows wxwidgets[webview] glew glfw3 eigen3 proj libgeotiff clfft boost opencl pcl liblas cuda ffmpeg[opencl,nvcodec,webp,zlib,vorbis,lzma,openh264] openimageio[gif,libraw] cryptopp --zip 
